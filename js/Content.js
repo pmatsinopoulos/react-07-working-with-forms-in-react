616d8d3a95ddd503c65b7eaf081c01e39c3b8df2
Panayotis Matsinopoulos <>
2019-07-15 13:03:10 +0300

    Input field becomes read-write as long as we have a dynamica way to set its value. Now based on state

--- js/Content.js
+++ js/Content.js
@@ -1,13 +1,23 @@
 class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Mr.2'
+    };
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+  }
+
+  handleTitleChange(e) {
+    console.debug(this, 'handleTitleChange', 'e', e);
+    this.setState({ title: e.target.value });
+  }
+
   render() {
     return React.createElement(
-      "div",
+      'div',
       null,
-      React.createElement(
-        "h1",
-        null,
-        "Hello World!"
-      )
+      React.createElement('input', { type: 'text', name: 'title', value: this.state.title,
+        onChange: this.handleTitleChange })
     );
   }
 }
--- jsx/Content.jsx
+++ jsx/Content.jsx
@@ -1,8 +1,22 @@
 class Content extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'Mr.2'
+    }
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+  }
+
+  handleTitleChange(e) {
+    console.debug(this, 'handleTitleChange', 'e', e)
+    this.setState({title: e.target.value})
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <input type="text" name="title" value={this.state.title}
+         onChange={this.handleTitleChange}/>
       </div>
     )
   }
